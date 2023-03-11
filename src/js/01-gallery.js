c6284b2e09985bdc72e3f512f6c550dac9fdbfee
MargaritaSukhova <margarytasu@gmail.com>
2023-03-11 23:32:05 +0200

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(photos) {
+  return photos
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>
+</div>`;
+    })
+    .join('');
+}
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
