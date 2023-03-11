0f6524bd0de00c18f3d9fff06a0554d97400c7d2
MargaritaSukhova <margarytasu@gmail.com>
2023-03-12 01:03:31 +0200

    task-3 beggining

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,14 @@
+// import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener(
+  'input',
+  (onFormInput = event => {
+    console.log(event.elements);
+  })
+);
+
+// function onFormInput(event) {
+//   console.log(event.elements);
+// }
