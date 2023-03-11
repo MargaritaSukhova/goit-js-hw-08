4e7b0921a5ae708b8d9baf4b2ba2d6988f163484
MargaritaSukhova <114304966+MargaritaSukhova@users.noreply.github.com>
2023-03-11 23:03:52 +0000

    Deploying to gh-pages from @ MargaritaSukhova/goit-js-hw-08@0f6524bd0de00c18f3d9fff06a0554d97400c7d2 🚀

---
+++ 03-feedback.2d59a3c7.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("input",onFormInput=e=>{console.log(e.elements)});
+//# sourceMappingURL=03-feedback.2d59a3c7.js.map
---
+++ 03-feedback.c1c22902.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("input",onFormInput=function(e){console.log(e.elements)});
+//# sourceMappingURL=03-feedback.c1c22902.js.map
